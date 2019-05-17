2a0eb7731634493a6e46c5a88f9e10e0ebc2c3d6
Dan Correa <dancorrea3@gmail.com>
2019-05-16 20:02:55 -0400

    moved module exports down

--- server.js
+++ server.js
@@ -2,8 +2,30 @@ var express = require("express");
 var path = require("path");
 var route = express.Router();
 
-module.exports = route;
 
 route.get('/', (req, res)); {
     res.send('/ being hit');
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = route;
