0040b1e98d217fd5c76bb73a454d3bdde782534a
Dan Correa <dancorrea3@gmail.com>
2019-05-19 00:19:46 -0400

    comment, line 7, server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 var bodyParser = require("body-parser");
 
 var PORT = process.env.PORT || 3000;
-
+// added body-parser on recommendation of TA; unsure of application
 app.use(bodyParser.urlencoded({
   extended: true
 }));
